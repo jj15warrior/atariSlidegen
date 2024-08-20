3c0946581ad20a566bd9755309e82fb6c2a9f38c
jj15warrior <janek302010@gmail.com>
2024-08-20 21:17:07 +0200

    lab lookup table

--- src/main.cpp
+++ src/main.cpp
@@ -16,141 +16,159 @@ typedef struct color{
 	unsigned char a;
 };
 
-unsigned char palette[] = {
-                4,4,4,
-                119,72,11,
-                113,60,45,
-                132,55,63,
-                128,48,111,
-                117,47,143,
-                100,52,164,
-                59,73,164,
-                41,86,143,
-                30,98,111,
-                26,109,63,
-                39,113,11,
-                57,108,0,
-                79,98,0,
-                101,86,0,
-                119,72,11
+const double paletteLAB[] = {
+    23.6714,0.537196,0.00159145,
+    90.594,6.47322,67.6177,
+    87.0895,20.9209,21.3131,
+    87.946,35.6443,7.45236,
+    86.7808,49.7505,-25.6815,
+    86.4125,53.4359,-42.8298,
+    87.1395,48.0619,-51.4168,
+    90.1326,17.5564,-47.0506,
+    91.9281,-3.68366,-34.9256,
+    93.6571,-24.5008,-16.216,
+    95.173,-46.4178,16.3455,
+    95.9557,-57.0706,71.0914,
+    95.7305,-47.6658,91.7539,
+    94.6902,-30.7413,91.8397,
+    93.0072,-13.2026,91.5156,
+    90.594,6.47322,67.6177
 };
 
-
-float distance(color a, color b){
-
-    float Ra = a.r / 255.0f;
-    float Ga = a.g / 255.0f;
-    float Ba = a.b / 255.0f;
-
-    float Rb = b.r / 255.0f;
-    float Gb = b.g / 255.0f;
-    float Bb = b.b / 255.0f;
-
-
-    if(Ra > 0.04045)
-        Ra = pow(((Ra + 0.055)/1.055), 2.4);
+double F(double input) 
+{
+    if (input > 0.008856)
+        return (pow(input, 0.333333333));
     else
-        Ra /= 12.92;
+        return ((841/108)*input + 4/29);
+}
+double distance(color a, color b){
 
-    if(Rb > 0.04045)
-        Rb = pow(((Rb + 0.055)/1.055), 2.4);
+    double Xa = 0.412453*a.r + 0.357580*a.g + 0.189423*a.b;
+    double Ya = 0.212671*a.r + 0.715160*a.g + 0.072169*a.b;
+    double Za = 0.019334*a.r + 0.119193*a.g + 0.950227*a.b;
+
+    double Xb = 0.412453*b.r + 0.357580*b.g + 0.189423*b.b; 
+    double Yb = 0.212671*b.r + 0.715160*b.g + 0.072169*b.b;
+    double Zb = 0.019334*b.r + 0.119193*b.g + 0.950227*b.b;
+
+    double Xo = 95.047;
+    double Yo = 100;
+    double Zo = 108.883;
+
+    Xa /= Xo;
+    Ya /= Yo;
+    Za /= Zo;
+
+    Xb /= Xo;
+    Yb /= Yo;
+    Zb /= Zo;
+
+
+
+    if (Xa > 0.008856)
+        Xa = cbrt(Xa);
     else
-        Rb /= 12.92;
-
-
-
-    if (Ga > 0.04045)
-        Ga = pow((( Ga + 0.055 ) / 1.055 ), 2.4);
-    else
-        Ga /= 12.92;
+        Xa = (double(841)/108)*Xa + double(4)/29;
     
-    if(Gb > 0.04045)
-        Gb = pow((( Gb + 0.055 ) / 1.055 ), 2.4);
+    if (Ya > 0.008856)
+        Ya = cbrt(Ya);
     else
-        Gb /= 12.92;
-
-
-    if(Ba > 0.04045)
-        Ba = pow((( Ba + 0.055 ) / 1.055 ), 2.4);
-    else
-        Ba /= 12.92;
-
-    if(Bb > 0.04045)
-        Bb = pow((( Bb + 0.055 ) / 1.055 ), 2.4);
-    else
-        Bb /= 12.92;
-
-    Ra *= 100;
-    Ga *= 100;
-    Ba *= 100;
-    Rb *= 100;
-    Gb *= 100;
-    Bb *= 100;
-
-    float Xa = Ra * 0.4124 + Ga * 0.3576 + Ba * 0.1805;
-    float Ya = Ra * 0.2126 + Ga * 0.7152 + Ba * 0.0722;
-    float Za = Ra * 0.0193 + Ga * 0.1192 + Ba * 0.9505;
-
-    float Xb = Rb * 0.4124 + Gb * 0.3576 + Bb * 0.1805;
-    float Yb = Rb * 0.2126 + Gb * 0.7152 + Bb * 0.0722;
-    float Zb = Rb * 0.0193 + Gb * 0.1192 + Bb * 0.9505;
-
+        Ya = (double(841)/108)*Ya + double(4)/29;
     
-    // reference illuminant F8 96.413 100.000 82.333
-    float var_Xa = Xa / 96.413;
-    float var_Ya = Ya / 100.0;
-    float var_Za = Za / 82.333;
+    if (Za > 0.008856)
+        Za = cbrt(Za);
+    else
+        Za = (double(841)/108)*Za + double(4)/29;
 
-    float var_Xb = Xb / 96.413;
-    float var_Yb = Yb / 100.0;
-    float var_Zb = Zb / 82.333;
 
-    if ( var_Xa > 0.008856 )    var_Xa = pow(var_Xa, ( 1/3 ));
-    else                        var_Xa = ( 7.787 * var_Xa ) + ( 16 / 116 );
-    if ( var_Ya > 0.008856 )    var_Ya = pow(var_Ya, ( 1/3 ));
-    else                        var_Ya = ( 7.787 * var_Ya ) + ( 16 / 116 );
-    if ( var_Za > 0.008856 )    var_Za = pow(var_Za, ( 1/3 ));
-    else                        var_Za = ( 7.787 * var_Za ) + ( 16 / 116 );
-
-    if ( var_Xb > 0.008856 )    var_Xb = pow(var_Xb, ( 1/3 ));
-    else                        var_Xb = ( 7.787 * var_Xb ) + ( 16 / 116 );
-    if ( var_Yb > 0.008856 )    var_Yb = pow(var_Yb, ( 1/3 ));
-    else                        var_Yb = ( 7.787 * var_Yb ) + ( 16 / 116 );
-    if ( var_Zb > 0.008856 )    var_Zb = pow(var_Zb, ( 1/3 ));
-    else                        var_Zb = ( 7.787 * var_Zb ) + ( 16 / 116 );
+    if (Xb > 0.008856)
+        Xb = cbrt(Xb);
+    else
+        Xb = (double(841)/108)*Xb + double(4)/29;
     
+    if (Yb > 0.008856)
+        Yb = cbrt(Yb);
+    else
+        Yb = (double(841)/108)*Yb + double(4)/29;
+    
+    if (Zb > 0.008856)
+        Zb = cbrt(Zb);
+    else
+        Zb = (double(841)/108)*Zb + double(4)/29;
+    
+    double La = 116 * Ya - 16;
+    double aa = 500 * (Xa- Ya);
+    double ba = 200 * (Ya - Za);
 
-    float cieLa = ( 116 * var_Ya ) - 16;
-    float cieAa = 500 * ( var_Xa - var_Ya );
-    float cieBa = 200 * ( var_Ya - var_Za );
+    double Lb = 116 * Yb - 16;
+    double ab = 500 * (Xb- Yb); 
+    double bb = 200 * (Yb - Zb);
 
-    float cieLb = ( 116 * var_Yb ) - 16;
-    float cieAb = 500 * ( var_Xb - var_Yb );
-    float cieBb = 200 * ( var_Yb - var_Zb );
 
-    cout << cieLa << " " << cieAa << " " << cieBa << endl;
+    return sqrt((La-Lb)*(La-Lb) + (aa-ab)*(aa-ab) + (ba-bb)*(ba-bb));
+}
+double distance(color a, double Lb, double ab, double bb){
+    double Xa = 0.412453*a.r + 0.357580*a.g + 0.189423*a.b;
+    double Ya = 0.212671*a.r + 0.715160*a.g + 0.072169*a.b;
+    double Za = 0.019334*a.r + 0.119193*a.g + 0.950227*a.b;
 
-    return sqrt(pow(cieLa - cieLb, 2) + pow(cieAa - cieAb, 2) + pow(cieBa - cieBb, 2));
+    double Xo = 95.047;
+    double Yo = 100;
+    double Zo = 108.883;
+
+    Xa /= Xo;
+    Ya /= Yo;
+    Za /= Zo;
+
+
+    if (Xa > 0.008856)
+        Xa = cbrt(Xa);
+    else
+        Xa = (double(841)/108)*Xa + double(4)/29;
+    
+    if (Ya > 0.008856)
+        Ya = cbrt(Ya);
+    else
+        Ya = (double(841)/108)*Ya + double(4)/29;
+    
+    if (Za > 0.008856)
+        Za = cbrt(Za);
+    else
+        Za = (double(841)/108)*Za + double(4)/29;
+
+
+    double La = 116 * Ya - 16;
+    double aa = 500 * (Xa- Ya);
+    double ba = 200 * (Ya - Za);
+
+
+
+
+    return sqrt((La-Lb)*(La-Lb) + (aa-ab)*(aa-ab) + (ba-bb)*(ba-bb));
 }
 
-
 color closest(color a){
-
+    short minidx = 0, mindist = SHRT_MAX;
+    short dist;
+    for(short i=0;i<16*3;i+=3){
+        dist = distance(a, paletteLAB[i], paletteLAB[i+1], paletteLAB[i+2]);
+        if(dist < mindist){
+            mindist = dist;
+            minidx = i;
+        }
+    }
+    return 
 }
 
 int main(){
-    unsigned char *data;
-    int w,h;
-    data = stbi_load("image.png", &w, &h, NULL, 4);
-    unsigned char data_copy[w*h*4];
-    memset(data_copy, 0, w*h*4);
+    //unsigned char *data;
+    int w=0,h=0;
+    //data = stbi_load("image.png", &w, &h, 0, 4);
+    //unsigned char data_copy[w*h*4];
     //resize to 640x480
-    stbir_resize_uint8_linear(data, w, h, 0, data_copy, 640, 480, 0, STBIR_RGBA);
+    //stbir_resize_uint8_linear(data, w, h, 0, data_copy, 640, 480, 0, STBIR_RGBA);
     
-    for(int i=0;i<16*3;i+=3){
-        distance(color{palette[i], palette[i+1], palette[i+2]}, color{palette[i], palette[i+1], palette[i+2]});
-    }
-
-    stbi_write_png("image2.png", 640,480, 4, data_copy, 0);
-    return 0;
+    
+    //stbi_write_png("image2.png", 640,480, 4, data_copy, 0);
 }
